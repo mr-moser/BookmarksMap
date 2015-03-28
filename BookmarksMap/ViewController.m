5e9e379a73117252c5841dc5d37e502da254ad3e
Moser <moser@Mosers-Mac-Pro.local>
2015-03-28 13:38:48 +0200

    Initial Commit

---
+++ BookmarksMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BookmarksMap
+//
+//  Created by Moser on 28/03/15.
+//  Copyright (c) 2015 Moser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
